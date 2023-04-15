67e6d81d083bb847700302dcfd9d4b4978e6df9b
Jacob Swindell <jacob.swindell@btinternet.com>
2023-04-15 16:23:24 +0100

    add map + robot state

--- lib/fleet_dashboard_web/live/fleet_live.ex
+++ lib/fleet_dashboard_web/live/fleet_live.ex
@@ -2,12 +2,18 @@ defmodule FleetDashboardWeb.FleetLive do
   use FleetDashboardWeb, :live_view
 
   def mount(_params, _session, socket) do
+    Phoenix.PubSub.subscribe(FleetMonitor.PubSub, "fleet_state")
     {:ok,
       socket
       |> assign(map_info: FleetMonitor.get_map_info())
       |> assign(image_topics: FleetMonitor.get_image_topics())
       |> assign_edges_and_verticies(FleetMonitor.get_nav_graph())
-      |> assign(locations: FleetMonitor.get_locations())}
+      |> assign(locations: FleetMonitor.get_locations())
+      |> assign(fleet_state: nil)}
+  end
+
+  def handle_info({:fleet_state, fleet_state}, socket) do
+    {:noreply, assign(socket, fleet_state: fleet_state)}
   end
 
   def assign_edges_and_verticies(socket, nav_graph) do
@@ -24,10 +30,28 @@ defmodule FleetDashboardWeb.FleetLive do
   end
 
   def x_rel(%{"width" => width, "scale" => scale}, x_meter) do
-    abs((x_meter / (width * scale)) * 100)
+    Float.round(abs((x_meter / (width * scale)) * 100), 3)
   end
 
   def y_rel(%{"height" => height, "scale" => scale}, y_meter) do
-    abs((y_meter / (height * scale)) * 100)
+    Float.round(abs((y_meter / (height * scale)) * 100), 3)
   end
+
+  def is_same_place?(%{"x" => x1, "y" => y1}, %{"x" => x2, "y" => y2}) do
+    x_diff = abs(abs(x1) - abs(x2))
+    y_diff = abs(abs(y1) - abs(y2))
+    x_diff < 0.3 and y_diff < 0.3
+  end
+
+  def shift_list([head | tail], new_val) do
+    [ new_val | shift_list(tail, head) ]
+  end
+  def shift_list([], _), do: []
+
+  def get_paths(robot_path, robot_location) do
+    Enum.zip(shift_list(robot_path, robot_location), robot_path)
+  end
+
+  def mode_colour(2), do: "bg-lime-400"
+  def mode_colour(_), do: "bg-[#E0E2DB]"
 end
